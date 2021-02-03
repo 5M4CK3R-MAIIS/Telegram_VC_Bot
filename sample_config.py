bde633cdaa8bc0e40a0daad3ace0b276527079ee
ahirearyan2 <66762638+ahirearyan2@users.noreply.github.com>
2021-02-03 15:38:57 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,4 +1,4 @@
-bot_token = "1488727769:AAElRU7yS309GP1t54F635ASDF"
+bot_token = "1519218188:AAHf-ftqzQzVvls-Eq5Z313kcjheIGBMt8Y"
 radio_link = "http://peridot.streamguys.com:7150/Mirchi"
-sudo_chat_id = -1001485393652
-owner_id = 1243703097
+sudo_chat_id = -1001338098714
+owner_id = 1388895264
